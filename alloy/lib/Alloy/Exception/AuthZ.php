487c0b49bc17ede7990a88d71f5c9a0206d9e53d
Maarten winter <mw@one.com>
2011-12-26 13:07:13 +0100

    Different exceptions for authorization and authentication problem

---
+++ alloy/lib/Alloy/Exception/AuthZ.php
@@ -0,0 +1,11 @@
+<?php
+namespace Alloy\Exception;
+
+/**
+ * Authorization Exception
+ *
+ * @package Alloy
+ * @link http://alloyframework.com/
+ * @license http://www.opensource.org/licenses/bsd-license.php
+ */
+class AuthZ extends \Exception {}
